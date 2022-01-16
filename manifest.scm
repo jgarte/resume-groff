24438f0ae21ea232cb3983895bff2aa413c53d82
jgart <47760695+jgarte@users.noreply.github.com>
2022-01-16 15:49:48 -0500

    Update manifest.scm

--- manifest.scm
+++ manifest.scm
@@ -1,11 +1,5 @@
 (specifications->manifest
-  '("python-flask"
-    "python-jinja2"
-    ;; "python-ansi2html"
-    ;; "python-giturlparse" Get this dep from guix-bioinformatics
-    ;; "python-dotenv"
-    ;; "python-tinydb"
-    "python-requests" 
-    "python-pygithub"
-    "python-gitpython"))
+  '("groff"    
+    "zathura"
+    "zathura-pdf-mupdf"))
 
